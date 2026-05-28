b4751e65bf5ae22bb6d0887caaf6711e371b919d
oscargarcia <ogarjae565@g.educaand.es>
2026-05-28 16:08:03 +0200

    Implementacion de la clase consola

---
+++ src/main/kotlin/Presentacion/Consola.kt
@@ -0,0 +1,92 @@
+package org.example.Presentacion
+
+import org.example.Modelo.Tipo
+
+object Consola {
+
+    fun solicitarOpcion(): String{
+
+        val opciones = listOf("1", "2", "3", "4", "5")
+        var opcion: String? = null
+        do {
+            mostrarMenu()
+            val entrada = readLine()
+            if (entrada in opciones) {
+                opcion = entrada
+            } else
+                println("Opción no válida.\n")
+        } while (opcion == null)
+        return opcion
+    }
+
+    fun solicitarNombrePokemon(): String {
+
+        var nombre: String? = null
+        do {
+            println("Introduce el nombre del pokemon que quieres registrar: ")
+            val entrada = readLine()?.trim()
+            if (!entrada.isNullOrBlank()) {
+                nombre = entrada
+            }
+        } while (nombre == null)
+        return nombre
+    }
+
+    fun obtenerTipos(): Pair<Tipo, Tipo> {
+        var tipo1: Tipo = Tipo.False
+
+        do {
+            tipo1 = solicitarTipo()
+            if (tipo1 == Tipo.False)
+                println("El tipo 1 no es válido")
+        } while (tipo1 == Tipo.False)
+
+        val tipo2: Tipo = solicitarTipo(tipo1)
+        val tipos: Pair<Tipo, Tipo> = tipo1 to tipo2
+        return tipos
+    }
+
+    private fun solicitarTipo(): Tipo {
+        val listaTipos = Tipo.values()
+        println("Introduce el primer tipo del Pokémon en formato 'Acero' :")
+        val entrada = readLine()?.trim()
+        listaTipos.forEach {
+            if (entrada == it.toString()){
+                return it
+            }
+        }
+        return Tipo.False
+    }
+
+    private fun solicitarTipo(tipo1: Tipo): Tipo {
+        val listaTipos = Tipo.values()
+        var tipo2: Tipo? = null
+
+        do {
+            println("Introduce el segundo tipo del Pokémon en formato 'Acero' (no repitas tipo) o 'False' en caso de que solo tenga un tipo:")
+            val entrada = readLine()?.trim()
+            listaTipos.forEach {
+                if (entrada == it.toString() && entrada != tipo1.toString()){
+                    tipo2 = it
+                }
+            }
+        } while (tipo2 == null)
+        return tipo2
+    }
+
+    private fun mostrarMenu(){
+        println("###################")
+        println("#     Pokedex     #")
+        println("###################")
+        println()
+        println("Elige una opción: ")
+        println()
+        println("1. Registrar un Pokémon")
+        println("2. Actualizar datos de un pokémon")
+        println("3. Eliminar Pokémon")
+        println("4. Listar Pokémon")
+        println("5. Salir")
+        println()
+        print("Tu opcion -> ")
+    }
+}
