36859530568abeaeaa20a061128a295c6e57ea43
oscargarcia <ogarjae565@g.educaand.es>
2026-05-28 16:06:19 +0200

    Creada la clase enum con los tipos posibles

---
+++ src/main/kotlin/Modelo/Tipo.kt
@@ -0,0 +1,23 @@
+package org.example.Modelo
+
+enum class Tipo{
+    Acero,
+    Agua,
+    Bicho,
+    Dragon,
+    Electrico,
+    Fantasma,
+    Fuego,
+    Hada,
+    Hielo,
+    Lucha,
+    Normal,
+    Planta,
+    Psiquico,
+    Roca,
+    Siniestro,
+    Tierra,
+    Veneno,
+    Volador,
+    False
+}
